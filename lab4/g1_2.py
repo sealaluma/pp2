8292acd53d5647bb1bdb056db01ff14bfe987948
sealaluma <116562964+sealaluma@users.noreply.github.com>
2022-11-27 21:02:04 +0600

    Update g1_2.py

--- lab4/g1_2.py
+++ lab4/g1_2.py
@@ -1,1 +1,17 @@
-# through iterator
+class gener:
+    def __init__(self, n):
+        self.n = n
+        self.num = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.num < self.n:
+            cur = self.num**2
+            self.num += 1
+            return cur
+        raise StopIteration()
+
+n = int(input())
+print(*gener(n))
